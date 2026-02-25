16c711bcb8020a1afba76e6121708ae67ec349a5
PunifX <hanniouiyassine.info@gmail.com>
2026-02-25 15:43:28 +0000

    Fix variable name typo and correct error message in Check_email function

--- Check_email.py
+++ Check_email.py
@@ -3,23 +3,25 @@ import sys
 
 
 def Check_email(domain):
-
-    spl_found = False
+    spf_found = False
+    
     try:
         records = dns.resolver.resolve(domain,"TXT")
         for record in records:
             #print(record)
             record = str(record)
             if record.startswith('"v=spf1') :
-                spl_found = True
-        if spl_found:
+                spf_found = True
+        if spf_found:
             print("[PASS] SPF record found")
         else:
             print("[FAIL] No SPF record")
-            print("you may be in danger dor spooofing attacks")
+            print("you may be in danger to spoofing attacks")
     except:
         print("[FAIL] No TXT records found")
         return       
+
+def check_dmarc(domain):
 
 
 if len(sys.argv) > 1:
