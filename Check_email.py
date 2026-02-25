8bf1ac07c826b67230c03dc82c3c08200059d123
PunifX <hanniouiyassine.info@gmail.com>
2026-02-25 16:02:43 +0000

    Refactor Check_dmarc function for consistency and improve code readability

--- Check_email.py
+++ Check_email.py
@@ -4,7 +4,7 @@ import sys
 
 def Check_email(domain):
     spf_found = False
-    
+
     try:
         records = dns.resolver.resolve(domain,"TXT")
         for record in records:
@@ -21,12 +21,29 @@ def Check_email(domain):
         print("[FAIL] No TXT records found")
         return       
 
-def check_dmarc(domain):
-
+def Check_dmarc(domain):
+    DMARC1 = False
+    try:
+        records = dns.resolver.resolve("_dmarc."+domain,"TXT")
+        for record in records:
+            #print(record)
+            record = str(record)
+            if record.startswith('"v=DMARC1') :
+                DMARC1 = True
+        if DMARC1:
+            print("[PASS] DMARC1 record found")
+        else:
+            print("[FAIL] No SPF record")
+            print("you may be in danger to spoofing attacks")
+    except:
+        print("[FAIL] No TXT records found")
+        return    
 
 if len(sys.argv) > 1:
     domain = str(sys.argv[1])
-    data = Check_email(domain)
+    Check_email(domain)
+    Check_dmarc(domain)
+
 
 else:
     print("Please enter a domain")
