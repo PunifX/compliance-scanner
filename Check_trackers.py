105a3c071243463ee54fbc42bed4a397181ee4e0
PunifX <hanniouiyassine.info@gmail.com>
2026-02-27 15:22:11 +0000

    Refactor Check_tracker function to improve variable naming and logic for tracker detection

--- Check_trackers.py
+++ Check_trackers.py
@@ -3,7 +3,7 @@ import sys
 from bs4 import BeautifulSoup
 tracker_domains = ["google-analytics.com", "googletagmanager.com", "facebook.net", "fbevents.js", "hotjar.com", "doubleclick.net", "googlesyndication.com", "twitter.com/uwt.js", "snap.licdn.com", "bat.bing.com", "analytics.tiktok.com", "cdn.mxpnl.com"]
 def Check_tracker(domain):
-    tracker = False
+    tracker_found = False
     tracker_list = []
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/120.0.0.0"}
 
@@ -11,14 +11,16 @@ def Check_tracker(domain):
     soup = BeautifulSoup(data.text,"html.parser")
     scripts = soup.find_all('script',src=True)
     for script in scripts:
-        if script in tracker_domains:
-            tracker = True
-            tracker_list.append(script)
+        script = script["src"]
+        for tracker in tracker_domains:
+            if tracker in script:
+                tracker_found=True
+                tracker_list.append(tracker)
     
-    if tracker:
+    if tracker_found:
         print("[FAIL] there is a tracker")
         print(tracker_list)
-    if not tracker:
+    if not tracker_found:
         print("[PASS] there is no tracker")
 
 
