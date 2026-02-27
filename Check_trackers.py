5130ec57b3bde60b774df92e74d975f6640136a0
PunifX <hanniouiyassine.info@gmail.com>
2026-02-27 15:15:36 +0000

    Refactor Check_tracker function to improve tracker detection logic and output messages

--- Check_trackers.py
+++ Check_trackers.py
@@ -1,16 +1,25 @@
 import requests
 import sys
 from bs4 import BeautifulSoup
-
+tracker_domains = ["google-analytics.com", "googletagmanager.com", "facebook.net", "fbevents.js", "hotjar.com", "doubleclick.net", "googlesyndication.com", "twitter.com/uwt.js", "snap.licdn.com", "bat.bing.com", "analytics.tiktok.com", "cdn.mxpnl.com"]
 def Check_tracker(domain):
-    
+    tracker = False
+    tracker_list = []
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/120.0.0.0"}
 
     data = requests.get(domain,headers=headers)
     soup = BeautifulSoup(data.text,"html.parser")
-    script = soup.find_all('script',src=True)
-    print(script)
+    scripts = soup.find_all('script',src=True)
+    for script in scripts:
+        if script in tracker_domains:
+            tracker = True
+            tracker_list.append(script)
     
+    if tracker:
+        print("[FAIL] there is a tracker")
+        print(tracker_list)
+    if not tracker:
+        print("[PASS] there is no tracker")
 
 
 if len(sys.argv) > 1:
